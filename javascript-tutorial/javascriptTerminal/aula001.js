8509143ef2ce67328eb2a789109b0f1181cc5581
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-01-09 21:00:29 -0300

    aula002

---
+++ javascript-tutorial/javascriptTerminal/aula001.js
@@ -0,0 +1,16 @@
+let nome = "Ana";
+let idade = 30;
+let aprovado = true;
+
+console.log(typeof(nome));
+console.log(typeof(idade));
+console.log(typeof(aprovado));
+
+console.log(`Olá ${nome} tudo bem com você`);
+console.log(`Sua idade e ${idade}`)
+
+if(aprovado == true){
+    console.log("Você esta aprovado");
+}else{
+    console.log("Você esta reprovado");
+}
---
+++ javascript-tutorial/javascriptTerminal/aula002.js
@@ -0,0 +1,6 @@
+let x = 5;
+let y = 5;
+
+let soma = x + y;
+
+console.log(soma);
