ca69c50af27cae2d04ec8abaac2f1d8af15a1383
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-01-10 20:44:54 -0300

    function

---
+++ javascript-tutorial/javascriptTerminal/aula003.js
@@ -0,0 +1,16 @@
+function saudacao(nome){
+    console.log(`Olá ${nome}`);
+}
+
+saudacao("Julia");
+
+function soma(a, b){
+    return a + b;
+}
+
+let resultado = soma(3, 2);
+console.log(resultado);
+
+const somar = (a, b) => {
+    return a + b;
+}
---
+++ javascript-tutorial/javascriptTerminal/aula004.js
@@ -0,0 +1,10 @@
+//Variável global
+let nome = "Jessica";
+
+function saudacao(){
+    //Variavel Local
+    let mensagem = "Olá ";
+    console.log(mensagem + nome);
+}
+
+saudacao();
