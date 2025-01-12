1a01c32d7a9b522f69ba657e0a229f3e2ffe6166
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-01-12 16:08:04 -0300

    aula 005

---
+++ javascript-tutorial/javascriptTerminal/aula005.js
@@ -0,0 +1,10 @@
+const nome = "julia";
+const idade = 30;
+
+const saudacao = `Eu me chamo ${nome.toUpperCase()}, tenho ${idade} anos`;
+
+console.log(saudacao);
+
+console.log(`Essa frase tem ${saudacao.length} caracteres`);
+
+console.log(saudacao.substring(0, 18));
