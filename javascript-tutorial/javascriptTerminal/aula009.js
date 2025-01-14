07fa9962a3d1b8ea5c4c0dd3ce09ff219b602b81
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-01-14 20:50:03 -0300

    aula009

---
+++ javascript-tutorial/javascriptTerminal/aula009.js
@@ -0,0 +1,9 @@
+const a = 40, b = 40;
+
+if(a == b){
+    console.log("a e igual a b!");
+}else if(a < b){
+    console.log("a e menor do que b");
+}else if(a > b){
+    console.log("a e maior do que b!");
+}
