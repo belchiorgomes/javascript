fd7d8cc51be9b2df6578563cb4c3491a84cc6c54
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-01-15 18:17:30 -0300

    operador ternário

---
+++ javascript-tutorial/javascriptTerminal/aula11.js
@@ -0,0 +1,16 @@
+let cor =  "amarelo";
+let numero;
+
+/*
+if(cor == "Vermelho"){
+    numero = 10;
+}else{
+    numero = 20;
+}
+
+console.log(numero);
+*/
+
+//operador ternário
+numero = cor == "vermelho" ? 10 : 20;
+console.log(numero);
