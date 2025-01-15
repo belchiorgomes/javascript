aa0d99929b1613fd31a32d673583879850ed325f
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-01-14 21:01:34 -0300

    exercicios

---
+++ javascript-tutorial/javascriptTerminal/aula10.js
@@ -0,0 +1,15 @@
+const cor = "Branco";
+
+switch (cor){
+    case "Branco":
+        console.log("A cor e Branca");
+        break;
+    case "Verde":
+        console.log("A cor e verde");
+        break;
+    case "rosa":
+        console.log("A cor e rosa");
+        break;
+    default:
+        console.log("Nenhuma das cores!");
+}
