d94a6b235edb3e893d7a49ffde2b3b25b1e43924
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-01-15 18:47:34 -0300

    while

---
+++ javascript-tutorial/javascriptTerminal/aula12.js
@@ -0,0 +1,15 @@
+let a = 0;
+while(a <= 5){
+    console.log(a);
+    a++;
+}
+
+let b = 1;
+while(b <= 10){
+    if(b == 5){
+        console.log(b);
+    }else{
+        console.log(b);
+    }
+    b++;
+}
