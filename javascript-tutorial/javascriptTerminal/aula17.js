7e1b58793a7470da27ab81055785a51be1907ca8
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-01-23 21:02:22 -0300

    operadores

---
+++ javascript-tutorial/javascriptTerminal/aula17.js
@@ -0,0 +1,20 @@
+// Operadores em JavaScript
+
+// Operadores aritmédicos
+
+let num1 = 3;
+let num2 = 2;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(++num1);
+console.log(--num2);
+
+// Operador de atribuição
+
+let a = 5;
+a += 5; // +=, -=, *=, /=, %=
+console.log(a);
