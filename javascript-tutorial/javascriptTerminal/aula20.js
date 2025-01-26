6d6c4e66c600092525587c04902c4df05dd67500
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-01-25 21:09:43 -0300

    aula20

--- javascript-tutorial/javascriptTerminal/aula20.js
+++ javascript-tutorial/javascriptTerminal/aula20.js
@@ -1,2 +1,5 @@
 // Crie um programa que peça ao usuário para digitar uma frase e conte quantas vezes a letra "a" aparece.
 
+const frase = "Esta e uma frase bem Bonida, feita para um exercicio de JavaScript";
+
+console.log(frase.charCodeAt());
