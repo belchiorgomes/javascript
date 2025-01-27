0e52c8a9156ed9c8673cddd4beec17e9530bf8e0
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-01-27 17:11:53 -0300

    exercicio 22

--- javascript-tutorial/javascriptTerminal/aula22.js
+++ javascript-tutorial/javascriptTerminal/aula22.js
@@ -1,18 +1,23 @@
-let diaSemana = new Date();
-let dia = diaSemana.getDate();
+let dataAtual = new Date();
+let diaSemana = dataAtual.getDay();
+//console.log(diaSemana);
 
-switch(dia){
-    case "Segunda" :
-    case "Terça" :
-    case "Quarta" :
-    case "Quinta" :
-    case "Sexta" :
-        console.log("Dia util da semana");
+let diasDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'];
+let hoje = diasDaSemana[diaSemana];
+//console.log(`Hoje e ${hoje}`);
+
+switch (hoje) {
+    case 'Segunda':
+    case 'Terça':
+    case 'Quarta':
+    case 'Qinta':
+    case 'Sexta':
+        console.log(`Hoje e ${hoje}, dia de ir trabalhar!`);
         break;
-    case "Sabado" :
-    case "Domingo" :
-        console.log("Final de semana");
+    case 'Sabado':
+    case 'Domingo':
+        console.log(`Hoje e ${hoje}, dia de folga, vamos descançar!`);
         break;
     default:
-        console.log("Dia da semana invalida");
+        console.log("Dia invalido");
 }
