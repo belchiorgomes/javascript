8eb0a30473c3be1b4c7897bb462181d8d2a3dfbe
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-02-12 12:35:35 -0300

    metodos de strings

--- javascript-tutorial/javascriptTerminal/metodosStrings/ex001.js
+++ javascript-tutorial/javascriptTerminal/metodosStrings/ex001.js
@@ -3,3 +3,13 @@
 let frase = "I love you";
 
 console.log(`A frase '${frase}' tem ${frase.length} caracteres!`);
+
+// O charAt()método retorna o caractere em um índice especificado (posição) em uma string:
+console.log(frase.charAt(3));
+
+// O charCodeAt()método retorna o código do caractere em um índice especificado em uma string:
+console.log(frase.charCodeAt(3));
+
+// O at()método retorna o caractere em um índice especificado (posição) em uma string.
+let letra = frase;
+console.log(`A frase '${frase}' o caractere na posição 3 e a letra ( ${letra.at(3)} )`);
