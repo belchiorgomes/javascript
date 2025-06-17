9dbf1263da5840f2439ccc4d9ef695a8729c8dd9
breno belchior de freitas gomes <131776313+belchiorgomes@users.noreply.github.com>
2025-06-17 12:23:10 -0300

    new ex

---
+++ tutorial/function/ex13.js
@@ -0,0 +1,17 @@
+let valor = 20;
+
+//função vazia
+function incremento(){
+    console.log("Estou dento da function")
+    valor += 30;
+};
+
+incremento();
+console.log(valor);
+
+//funçao com paramentos
+function mostrarNome(nome){
+    console.log(nome)
+};
+
+mostrarNome('BRENO');
